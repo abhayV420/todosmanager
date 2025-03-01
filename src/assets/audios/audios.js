458da50a5eea1fb61ff58761c1a97509a1058939
abhayV420e <abhayV420@outlook.com>
2025-03-01 18:46:48 +0530

    fixed an error

--- src/assets/audios/audios.js
+++ src/assets/audios/audios.js
@@ -3,6 +3,6 @@ import editAudio from './edit.mp3';
 import deleteAudio from './delete.mp3';
 import toggleAudio from './toggle.mp3';
 import errorAudio from './error.mp3';
-import notComp from './notComp.mp3';
+import notComp from './notcomp.mp3';
 
 export const audios = { addAudio, editAudio, deleteAudio, toggleAudio, errorAudio, notComp };
